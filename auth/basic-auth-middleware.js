42ea69d5007a3c8e2c5a8839793b293d876d427d
mai <mai.yusuf28@yahoo.com>
2020-02-04 17:36:34 +0200

    oauth middleware doone

---
+++ auth/basic-auth-middleware.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const base64 = require('base-64');
+const users = require('./userModel');
+const jwt = require('jsonwebtoken');
+
+
+function generateToken(user) {
+  let token = jwt.sign({ id: user._id  }, process.env.SECRET);
+  return token;
+}
+
+
+module.exports = (req, res, next) => {
+
+  if(!req.headers.authorization) { next('invalid login'); return; }
+
+  //   split the username and passowred from the header
+  let basic = req.headers.authorization.split(' ').pop();
+  //   let basic = req.headers.authorization.split(' : ')[1]
+
+
+
+  // split username and password from each other 
+  let [user, password] = base64.decode(basic).split(':');
+
+
+  users.authenticateBasic(user, password)
+    .then(validUser => {
+      console.log('validUser', validUser)
+      req.token = generateToken(validUser);
+      console.log('token:', req.token);
+      next();
+    })
+    .catch( err => next('invalid login2'));
+};
---
+++ src/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const basicAuth = require('../auth/basic-auth-middleware.js');
+const model=require('../model/model.js');
+const users = require('../auth/userModel.js');
+// const mongoose = require('mongoose');
+const oauth = require('../auth/Ouath-middleware.js');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+
+
+
+app.post('/signup', (req, res) => {
+
+  new users(req.body).save()
+    .then((user) => {
+      console.log('user',user);
+      let token = user.generateToken();
+      res.status(200).send(token);
+    }).catch(err => console.error(err));
+});
+
+app.post('/signin',basicAuth, (req, res) => {
+  res.status(200).send(req.token);
+});
+
+app.get('/oauth', oauth, (req, res) => {
+    res.status(200).send(req.token);
+  });
+
+
+module.exports = {
+  server: app,
+  start: port => {
+    let PORT = port || process.env.PORT || 3000;
+    app.listen(PORT, () => console.log('i am alive :', PORT));
+  }
+};
